e113bc7b39d0f10db0d362bdea1f8eadc4021df8
Stuart B. Wilkins <swilkins@bnl.gov>
2019-02-17 09:59:56 -0500

    Missed %

--- ci/setup_epics.bat
+++ ci/setup_epics.bat
@@ -3,5 +3,5 @@ set PATH=C:\Strawberry\perl\bin;%PATH%
 rem %PATH%
 
 rem Execute the Visual Studio batch file for 64-bit builds
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" %VC_ARCH
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" %VC_ARCH%
 
